aab0bd6de3f7c3f30a4b665eeffb7b4fc2902ae5
IamXGW <xuguangwei@Xus-MacBook-Pro.local>
2025-03-16 21:36:19 +0800

    singleton pattern

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DesignPatterns
-Design Patterns
+这是我学习设计模式时的代码实现。学习笔记见[我的博客](https://iamxgw.com)
---
+++ src/main/java/com/iamxgw/singleton_pattern/SingletonPattern.java
@@ -0,0 +1,10 @@
+package main.java.com.iamxgw.singleton_pattern;
+
+/**
+ * @description: 单例模式
+ * @author: IamXGW
+ * @create: 2025-03-16 21:30
+ */
+public class SingletonPattern {
+
+}
